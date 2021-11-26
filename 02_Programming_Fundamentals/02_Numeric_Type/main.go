3309d9dbc198e2419439b1c5fe8cff45815d7951
Adeh <adeh@Adeh.local>
2021-11-26 20:34:30 +0100

    You're doing great

--- 02_Programming_Fundamentals/02_Numeric_Type/main.go
+++ 02_Programming_Fundamentals/02_Numeric_Type/main.go
@@ -2,11 +2,33 @@ package main
 
 import "fmt"
 
+var a uint
+var b uint8
+var x int
+var y int8
+var z float64
+
 func main() {
-	x := 7
-	y := 7.23
+	a = 7
+	b = 128
+	x = -7
+	y = -128
+	z = 9.24
+
+	fmt.Println("a =", a)
+	fmt.Printf("%T\n", a)
+	fmt.Println("b =", b)
+	fmt.Printf("%T\n", b)
+
+	fmt.Println("-")
+
 	fmt.Println("x =", x)
 	fmt.Printf("%T\n", x)
 	fmt.Println("y =", y)
 	fmt.Printf("%T\n", y)
+
+	fmt.Println("-")
+
+	fmt.Println("z =", z)
+	fmt.Printf("%T\n", z)
 }
