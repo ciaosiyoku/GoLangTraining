a15cc00364427221bc9deb764e3f00c25b08d9b9
Adeh <adeh@Adeh.local>
2021-11-26 22:55:40 +0100

    You're doing great

--- 02_Programming_Fundamentals/03_String_Type/main.go
+++ 02_Programming_Fundamentals/03_String_Type/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("You're doing great")
+	s := "Hello Adeh"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	fmt.Println("")
+
+	// Show "Hello Adeh" in Decimal base 10
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+
+	fmt.Println("")
+
+	// Show "Hello Adeh" in UTF8
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U\n", s[i])
+	}
+
+	fmt.Println("")
+
+	// Show "Hello Adeh" in Hexa-decimal
+	for i, v := range s {
+		fmt.Printf("At index position %d we have hex %#x\n", i, v)
+	}
 }
