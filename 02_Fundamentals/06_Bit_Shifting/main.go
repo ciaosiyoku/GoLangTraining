0a3a03fa2ddf8a8536e905c68305070aacd4cec2
Adeh <adeh@Adeh.local>
2021-11-28 01:16:45 +0100

    You're doing great

--- 02_Fundamentals/06_Bit_Shifting/main.go
+++ 02_Fundamentals/06_Bit_Shifting/main.go
@@ -4,12 +4,37 @@ import (
 	"fmt"
 )
 
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
+
 func main() {
-	x := 2
+	fmt.Println("")
+
+	fmt.Printf("%d\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t%b\n", gb, gb)
 
 	fmt.Println("")
 
+	x := 8
 	fmt.Printf("%d\t\t%b\n", x, x)
+
+	y := x << 1
+	fmt.Printf("%d\t\t%b\n", y, y)
+
+	fmt.Println("")
+
+	kb2 := 1024
+	mb2 := 1024 * kb2
+	gb2 := 1024 * mb2
+
+	fmt.Printf("%d\t\t%b\n", kb2, kb2)
+	fmt.Printf("%d\t\t%b\n", mb2, mb2)
+	fmt.Printf("%d\t%b\n", gb2, gb2)
 
 	fmt.Println("")
 }
